e26ca22f895c78cd6394c1f61f66ccf558565647
yozhang <major_seventh@outlook.com>
2023-09-07 17:25:46 +0800

    cpp helloworld

---
+++ cpp/cpp-helloworld/helloworld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    cout<<a+2<<endl;
+    cout<<4;
+    int n=0;
+    printf("%d", n);
+    scanf("%d", &n);
+    printf("%d", n);
+    return 0;
+}
